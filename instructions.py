ba972d437c7847ac1017638f3cbdf54e16491e2c
Huiting Liu <liuhuiting@Huitings-MBP.wv.cc.cmu.edu>
2017-11-08 12:47:48 -0500

    add BFS for brute force search

--- instructions.py
+++ instructions.py
@@ -3,11 +3,13 @@ import itertools
 import timeout_decorator
 import json
 import sys
+import random
+from Queue import Queue
 
 ROUND_DIGIT = 10
 MAX_SEQ_LEN = 10
 MAX_ARG = 1e20
-PRUNE_UNK_NUMBER_STEP = 3
+PRUNE_UNK_NUMBER_STEP = 10
 PRUNE_RATIONALE_COVERAGE = 0
 CONSTANTS = [0.0, 0.01, 1000.0, 3.14, 60.0, 24.0]
 PARTITION_NUM = 6
@@ -42,8 +44,7 @@ class Corpus(object):
                        'rationale': d2['rationale'], 'correct': d1['correct'],
                        'raw': d2}
 
-    def findPathByVal(self, question_toks, rationale_toks, ans_text, verbose = False):
-
+    def findPathByVal(self, question_toks, rationale_toks, ans_text, options, verbose = False, search_type = 'DFS'):
         values = set(CONSTANTS)
         rationale_num = set()
         #print ans_text
@@ -73,13 +74,56 @@ class Corpus(object):
             print("Rationale Intermediates:",rationale_num)
             print("Ans_num:", ans)
 
-        optimal = [len(rationale_num), None]
+        if search_type == 'DFS':
+            optimal = [len(rationale_num), None]
 
-        path = self.DFS([], operations_list, values, rationale_num, ans, optimal, 0)
-        if path == None:
-            return optimal[1]
+            path = self.DFS([], operations_list, values, rationale_num, ans, optimal, 0)
+            if path == None:
+                return optimal[1]
+            else:
+                return path
         else:
-            return path
+            path, choice_idx = self.BFS([], operations_list, values, options)
+            return path, choice_idx
+
+    def BFS(self, path, ops, all_args, options):
+        queue = Queue()
+        step = 0
+        for op in ops:
+            args_num = argsNum[op]
+            for args in itertools.permutations(all_args, args_num):
+                ok, res = eval(op)(*args)
+                if ok and res < MAX_ARG:
+                    res = round(res, ROUND_DIGIT)
+                    if res in options:
+                        return path + [(op, args, res)], options.index(res)
+
+                    queue.put((step, path[:] + [(op, args, res)], all_args | set([res])))
+
+
+        while queue.empty() != False:
+            (step, cur_path, cur_args) = queue.get()
+
+            # random guess
+            if step > PRUNE_UNK_NUMBER_STEP:
+                idx = random.randint(0,len(options)-1)
+                return ["Random Guess:", options[idx]], idx
+
+            for op in ops:
+                args_num = argsNum[op]
+                for args in itertools.permutations(cur_args, args_num):
+                    ok, res = eval(op)(*args)
+                    if ok and res < MAX_ARG:
+                        res = round(res, ROUND_DIGIT)
+                        if res in options:
+                            return cur_path + [(op, args, res)], options.index(res)
+
+                        queue.put((step, cur_path[:] + [(op, args, res)], cur_args | set([res])))
+
+        # Random Guess
+        idx = random.randint(0,len(options)-1)
+        return ["Random Guess:", options[idx]], idx
+
 
     def DFS(self, path, ops, all_args, rationale, ans, optimal, unk_step):
         if len(rationale) < optimal[0]:
@@ -135,37 +179,56 @@ class Corpus(object):
             path = self.findPathByVal(question_toks, rationale_toks, ans_text, verbose)
             return path, confident
 
-    def findPath(self, timeout=None, verbose = False):
+    def findPath(self, timeout=None, verbose = False, search_type = 'DFS'):
         paths = []
         if timeout is None:
-            def find_path(question_toks, rationale_toks, ans_text):
-                return self.findPathByVal(question_toks, rationale_toks, ans_text, verbose)
+            def find_path(question_toks, rationale_toks, ans_text, options, search_type = 'DFS'):
+                return self.findPathByVal(question_toks, rationale_toks, ans_text, options, verbose, search_type)
         else:
             @timeout_decorator.timeout(timeout, use_signals=False)
-            def find_path(question_toks, rationale_toks, ans_text):
-                return self.findPathByVal(question_toks, rationale_toks, ans_text, verbose)
+            def find_path(question_toks, rationale_toks, ans_text, options, search_type = 'DFS'):
+                return self.findPathByVal(question_toks, rationale_toks, ans_text, options, verbose, search_type)
 
         confident_count = 0
+        correct_count = 0
+        total_count = 0
         for (i, record) in enumerate(self.data):
-            if i % PARTITION_NUM != self.partitionID:
-                continue
+            total_count += 1
+            #if i % PARTITION_NUM != self.partitionID:
+            #    continue
             question_toks = tokenize(record['question'])
             rationale_toks = tokenize(record['rationale'])
             correct_idx = ord(record['correct']) - ord('A')
             ans_text = record['options'][correct_idx][2:]
             _, ans = STR2FLOAT(ans_text)
             confident = ans is not None
+
+            options = []
+            for option in record['options']:
+                opt_text = option[2:]
+                flag, opt = STR2FLOAT(opt_text)
+                if flag:
+                    options.append(opt)
+
             if verbose:
                 print("Question %d: %s" % (i, record['question']))
                 print("Rationale: %s" % (record['rationale']))
                 print("Answer: %s" % (ans_text))
             result = dict(record['raw'])
             try:
-                result['path'] = find_path(question_toks, rationale_toks, ans_text)
-                result['confident'] = confident
-                confident_count += 1
+                if search_type == 'DFS':
+                    result['path'] = find_path(question_toks, rationale_toks, ans_text, options, search_type)
+                    result['confident'] = confident
+                    confident_count += 1
+                else:
+                    path, choice_idx = find_path(question_toks, rationale_toks, ans_text, options, search_type)
+                    result['path'] = path
+                    result['choice'] = chr(ord('A') + choice_idx)
+                    if choice_idx == correct_idx:
+                        correct_count += 1
             except: pass
             print json.dumps(result)
+        print(correct_count, correct_count * 1.0 / total_count)
             #print("%d Done!" % (i))
         #return paths
 
@@ -179,7 +242,7 @@ def main():
         print("please provide partition")
         exit(1)
     corpus = Corpus(dataset='dev', partition_id=partition)
-    corpus.findPath(timeout=3)
+    corpus.findPath(search_type='BFS')
     '''
     fail = []
     count = 0
