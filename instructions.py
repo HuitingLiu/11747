6b03bb59c6e2c9fe722c6ce8e0ee89cd790d14d1
Huiting Liu <liuhuiting@Huitings-MBP.wv.cc.cmu.edu>
2017-11-08 15:16:53 -0500

    refactor BFS

--- instructions.py
+++ instructions.py
@@ -92,8 +92,8 @@ class Corpus(object):
                 values.add(round(res, ROUND_DIGIT))
 
         if search_type == 'BFS':
-            path, choice_idx = self.BFSSolver([], operations_list, values, options)
-            return path, choice_idx
+            path, choice_idx, hit = self.BFSSolver([], operations_list, values, options)
+            return path, choice_idx, hit
 
     def BFSSolver(self, path, ops, all_args, options):
         queue = Queue()
@@ -105,7 +105,7 @@ class Corpus(object):
                 if ok and res < MAX_ARG:
                     res = round(res, ROUND_DIGIT)
                     if res in options:
-                        return path + [(op, args, res)], options.index(res)
+                        return path + [(op, args, res)], options.index(res), True
 
                     queue.put((step, path[:] + [(op, args, res)], all_args | set([res])))
 
@@ -124,12 +124,12 @@ class Corpus(object):
             if step > 1 and queue.qsize() > 30000:
                 # Random Guess
                 idx = random.randint(0,len(options)-1)
-                return ["Random Guess:", options[idx]], idx
+                return ["Random Guess:", options[idx]], idx, False
 
             # Cut complicate branch
             if step > 3:
                 idx = random.randint(0,len(options)-1)
-                return ["Random Guess:", options[idx]], idx
+                return ["Random Guess:", options[idx]], idx, False
 
             for op in ops:
                 args_num = argsNum[op]
@@ -138,13 +138,13 @@ class Corpus(object):
                     if ok and res < MAX_ARG:
                         res = round(res, ROUND_DIGIT)
                         if res in options:
-                            return cur_path + [(op, args, res)], options.index(res)
+                            return cur_path + [(op, args, res)], options.index(res), True
 
                         queue.put((step + 1, cur_path[:] + [(op, args, res)], cur_args | set([res])))
 
         # Random Guess
         idx = random.randint(0,len(options)-1)
-        return ["Random Guess:", options[idx]], idx
+        return ["Random Guess:", options[idx]], idx, False
 
 
     def DFS(self, path, ops, all_args, rationale, ans, optimal, unk_step):
@@ -208,6 +208,8 @@ class Corpus(object):
 
         correct_count = 0
         total_count = 0
+        hit_count = 0
+        hit_correct = 0
         for (i, record) in enumerate(self.data):
             total_count += 1
             question_toks = tokenize(record['question'])
@@ -222,6 +224,7 @@ class Corpus(object):
                 choice_idx = random.randint(0, 4)
                 result['path'] = ["Random Guess for no-numeric question"]
                 result['choice'] = chr(ord('A') + choice_idx)
+                result['hit'] = False
                 if choice_idx == correct_idx:
                     correct_count += 1
             else:
@@ -233,14 +236,20 @@ class Corpus(object):
                         options.append(opt)
 
                 try:
-                    path, choice_idx = find_ans(question_toks, options, search_type)
+                    path, choice_idx, hit = find_ans(question_toks, options, search_type)
                     result['path'] = path
                     result['choice'] = chr(ord('A') + choice_idx)
+                    result['hit'] = hit
+                    if hit:
+                        hit_count += 1
+
                     if choice_idx == correct_idx:
                         correct_count += 1
+                        if hit:
+                            hit_correct += 1
                 except: pass
             print json.dumps(result)
-        print(correct_count, correct_count * 1.0 / total_count)
+        print("Correct Count:", correct_count, " Correct Rate:", correct_count * 1.0 / total_count, " Hit Correct:", hit_correct, " Hit Total:", hit_count, " Hit Correct Rate:",hit_correct * 1.0 / hit_count)
 
 
     def findPath(self, timeout=None, verbose = False):
